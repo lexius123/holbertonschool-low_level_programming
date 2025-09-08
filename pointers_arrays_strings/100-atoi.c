4b75b1f9d2f3f8b8ef9700ab8d229cb4b5a6f010
lexius123 <jesujesujesu123456@gmail.com>
2025-09-08 05:22:41 +0000

    Add 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -9,37 +8,25 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int i = 0, sign = 1, started = 0;
+	unsigned int num = 0;
 
-	/* recorrer el string */
 	while (s[i] != '\0')
 	{
 		/* contar signos */
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] == '+')
-			sign *= 1;
 
-		/* si es dígito */
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			/* convertir y acumular con signo */
-			int digit = s[i] - '0';
-
-			/* overflow check */
-			if (sign == 1 && (result > (INT_MAX - digit) / 10))
-				return (INT_MAX);
-			if (sign == -1 && (result < (INT_MIN + digit) / 10))
-				return (INT_MIN);
-
-			result = result * 10 + (sign * digit);
+			started = 1;
+			num = (num * 10) + (s[i] - '0');
 		}
-		/* si ya empezó número y se acaba */
-		else if (result != 0)
+		else if (started)
 			break;
 
 		i++;
 	}
 
-	return (result);
+	return (num * sign);
 }
