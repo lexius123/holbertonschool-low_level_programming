6d409de2af5ffd798dab93267b2dc50e171b0911
lexius123 <jesujesujesu123456@gmail.com>
2025-09-29 04:15:29 +0000

    soluciones malloc_free

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string in new memory
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string, or NULL if fail
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return (dup);
+}
