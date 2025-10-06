929533ef093888b1669fa81b5ef980a71a90a820
lexius123 <jesujesujesu123456@gmail.com>
2025-10-06 03:09:52 +0000

    add malloc_checked, string_nconcat, _calloc and array_range functions

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ * If malloc fails, exit with status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
